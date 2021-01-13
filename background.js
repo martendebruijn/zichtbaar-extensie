7c9f59a4dcfa5a459f95a3ad8f1996b36fc68ace
Marten de Bruijn <martenbruijn@gmail.com>
2021-01-13 15:45:30 +0100

    commands api working

--- background.js
+++ background.js
@@ -26,3 +26,17 @@ chrome.runtime.onMessage.addListener(function (msg, sender, sendResponse) {
     console.log(msg);
   }
 });
+
+// Listening to commands
+// As the manifest key name implies, suggested_key is only a suggestion for key binding.
+// That suggestion will be taken into consideration only if the key is not already assigned
+// to another command.
+// Another thing to have in mind is that suggested keys are only considered when an extension is
+// first installed. Disabling and enabling, updating, or reloading won't have any effect. So if
+// you just changed the suggested key in your manifest and want to test it, you have to uninstall
+// the extension and install it again.
+// Works for me tho ^ just updating works
+// But you do have to re-install the whole thing if you didn't had commands before
+chrome.commands.onCommand.addListener(function (command) {
+  console.log('Command:', command);
+});
