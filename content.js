d88d31d950313e61639cab3be6e8cae0befa92ee
Marten de Bruijn <martenbruijn@gmail.com>
2021-01-13 22:02:07 +0100

    worked on nav feature

--- content.js
+++ content.js
@@ -1,169 +1,101 @@
 // --- NAV ---
-// check if <header> exist
-// function getHeader() {
-//   const header = document.querySelector('header');
-//   // ga ervan uit dat de 1e gevonden header element, de header is van de pagina
-//   if (header) {
-//     console.log({ State: 'header found', data: header });
-//     navInHeader(header);
-//   } else {
-//     console.log({ State: 'header NOT found', data: null });
-//     filterNavs();
-//   }
-// }
-var gotHeader;
-function getHeader() {
+function getMainNav() {
   const header = document.querySelector('header');
   if (header) {
     gotHeader = true;
     return navInHeader(header);
   } else {
     gotHeader = false;
-    return getAllNavs();
+    return getAllNavs(false);
   }
 }
-
-// check if <nav> exist in <header>
-// function navInHeader(header) {
-//   const navs = header.querySelectorAll('header nav');
-//   if (navs.length > 0) {
-//     console.log({ State: 'navs found in header', data: navs });
-//   } else {
-//     console.log({ State: 'navs NOT found in header', data: null });
-//     filterNavs();
-//   }
-// }
 function navInHeader(header) {
   const navs = header.querySelectorAll('header nav');
-  // false => ul in header
-  return navs.length > 0 ? navs : getAllNavs();
+  return navs.length > 0 ? navs : getAllNavs(true);
 }
-
-// check if <nav> exist outside <header>
-function getAllNavs() {
+function getAllNavs(scopeHeader) {
   const navs = document.querySelectorAll('nav');
   const arr = Array.from(navs);
   if (arr.length > 0) {
     return getFooterNavs(arr);
-  } else if (gotHeader) {
-    getUlInHeader();
+  } else if (scopeHeader) {
+    return getUlInHeader();
   } else {
-    getAllUls();
+    return getAllUls();
   }
-
-  return arr.length > 0 ? getFooterNavs(arr) : null;
 }
-
-// get Navs in Footer
 function getFooterNavs(allNavs) {
   const navsInFooter = document.querySelectorAll('footer nav');
   const arr = Array.from(navsInFooter);
   return arr.length > 0 ? filterNavs(allNavs, arr) : allNavs;
 }
-
-// filter out footer navs
 function filterNavs(allNavs, footerNavs) {
-  // https://stackoverflow.com/questions/34901593/how-to-filter-an-array-from-all-elements-of-another-array
+  // Source: https://stackoverflow.com/questions/34901593/how-to-filter-an-array-from-all-elements-of-another-array
   const filtered = allNavs.filter(function (e) {
     return this.indexOf(e) < 0;
   }, footerNavs);
-  // null => give err
-  return filtered.length > 0 ? filtered : null;
+  return filtered.length > 0 ? filtered : noNav();
 }
-
-// get footers elements
-function getFooters() {
-  const footers = document.querySelectorAll('footer');
-  if (footers.length < 2) {
-    console.log({ State: 'one footer found', data: footers });
-  } else {
-    console.log({ State: 'multiple footers found', data: footers });
-    // check which one is the one of the page
-    // commonly the one at the bottom / not inside an article
-  }
-}
-
 function getAllUls() {
   const uls = document.querySelectorAll('ul');
   const arr = Array.from(uls);
-  // null => give error
-  return arr.length > 0 ? checkUlForLinks(arr) : null;
+  return arr.length > 0 ? checkUlForLinks(arr, false) : noNav();
 }
 function getUlInHeader() {
   const uls = document.querySelectorAll('header ul');
   const arr = Array.from(uls);
-  return arr.length > 0 ? checkUlForLinks(arr) : getAllUls();
+  return arr.length > 0 ? checkUlForLinks(arr, true) : getAllUls();
 }
-function checkUlForLinks(uls) {
+function checkUlForLinks(uls, scopeHeader) {
   const filtered = uls.filter(function (ul) {
     const gotLinks = ul.querySelector('a');
-    if (gotLinks) {
-      return ul;
-    }
+    if (gotLinks) return ul;
   });
-  // null => give error
-  return filtered.length > 0 ? filtered : null;
-}
 
-function getUlInFooter() {
-  const uls = document.querySelectorAll('footer ul');
-  console.log(uls);
-}
-
-// 1. nav in header
-// checkHeader() => checkHeaderNav()
-// 2. nav outside header (and not in footer)
-// checkHeader() => checkNav()
-// 3. list in header
-// checkHeader() => checkHeaderNav() => checkHeaderLinkList()
-// 4. list outside header
-// checkHeader() => checkNav() => checkLinkList()
-function getMainNav() {
-  const header = getHeader();
-
-  console.log(header);
-}
-getMainNav();
-
-function _getMainNav() {
-  if (getHeader()) {
-    if (navInHeader()) {
-      console.log('navs in header');
-    } else {
-      if (filteredNavs()) {
-        console.log('navs found on page, filtered out footer-navs');
-      } else {
-        const uls = getUlInHeader();
-        const gotLinks = checkUlForLinks(uls);
-        if (gotLinks) {
-          console.log('Ul link list found in header');
-        } else {
-          const allUls = getAllUls();
-          const _gotLinks = checkUlForLinks(allUls);
-          if (_gotLinks) {
-            console.log('found link list on page');
-            // filter out footer ones
-          } else {
-            console.log("can't find navigation");
-          }
-        }
-      }
-    }
-  } else {
-    if (filteredNavs()) {
-      console.log('found navs on page (filtered out the ones in the footer)');
-    } else {
-      const uls = getAllUls();
-      const gotLinks = checkUlForLinks(uls);
-      if (gotLinks) {
-        console.log('found ul link list on page');
-      } else {
-        console.log("can't find navigation");
-      }
-    }
+  if (filtered.length > 0 && scopeHeader) {
+    return filtered;
+  } else if (filtered.length === 0 && scopeHeader) {
+    return getAllUls();
+  } else if (filtered.length > 0 && !scopeHeader) {
+    return filterUl();
   }
 }
-// _getMainNav();
+function getFooterUls(allUls) {
+  const ulsInFooter = document.querySelectorAll('footer ul');
+  const arr = Array.from(ulsInFooter);
+  return arr.length > 0 ? filterUl(ulsInFooter, arr) : allUls;
+}
+function filterUl(uls, footerUls) {
+  const filtered = uls.filter(function (e) {
+    return this.indexOf(e) < 0;
+  }, footerUls);
+  return filtered.length > 0 ? filtered : noNav();
+}
+function noNav() {
+  console.log("Can't find navigation...");
+}
+function removeHidden() {
+  const mainNav = getMainNav();
+  let index = mainNav.length - 1;
+  while (index >= 0) {
+    var style = window.getComputedStyle(mainNav[index]);
+    let display = style.getPropertyValue('display'),
+      visibility = style.getPropertyValue('visibility'),
+      opacity = style.getPropertyValue('opacity');
+    if (display === 'none' || visibility === 'hidden' || opacity === 0) {
+      mainNav.splice(index, 1);
+    }
+    index -= 1;
+  }
+  return mainNav;
+}
+const __nav = removeHidden();
+console.log(__nav[0]);
+__nav[0].focus();
+// .focus() only works when the element is focusable.
+//  to make a non-focusable element focusable we have to use tabindex SO =>
+// first check tabindex (why? if we want it to be focusable, it is going to be focusable)
+// add/change tabindex (tabindex = -1 , i believe?)
 
 // --- LANGUAGE ---
 function getLang() {
